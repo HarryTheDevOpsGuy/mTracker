af864e15b20c371a311604a4538a6d92034242b7
Harry <mr.harry41@gmail.com>
2022-07-30 15:14:44 +0530

    Created New Release - Version v0.2.7

--- mtracker.sh
+++ mtracker.sh
@@ -4,15 +4,10 @@
 # export SLACK_CHANNEL="#devops"
 
 export SSH_SESSION_ID="MT-${RANDOM}"
-#export PROMPT_COMMAND='RETRN_VAL=$?;logger -p local6.debug "$(whoami) [${SSH_SESSION_ID}]: $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [$RETRN_VAL]"'
+export PROMPT_COMMAND='RETRN_VAL=$?;logger -p local6.debug "$(whoami) [${SSH_SESSION_ID}]: $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [$RETRN_VAL]"'
 
 MTRACKER_BIN="$(command -v mtracker)"
 if [[ ! -z ${MTRACKER_BIN} ]]; then
   ${MTRACKER_BIN} -m auth
   trap "${MTRACKER_BIN} -m history" 0
 fi
-
-
-sudo cat <<EOT >> /etc/environment
-PROMPT_COMMAND='RETRN_VAL=\$?;logger -p local6.debug "\$(whoami) [\${SSH_SESSION_ID:-MT-00}]: \$(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [\$RETRN_VAL]"'
-EOT
