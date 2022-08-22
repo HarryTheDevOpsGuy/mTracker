943873a37e5d6e86e7d8d284dde503f08519c41c
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-22 08:30:35 +0530

    Update cron.sh

--- src/cron.sh
+++ src/cron.sh
@@ -2,4 +2,6 @@ SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #Monitor System with mTracker
 */2 * * * *   root   mtracker -m mwatcher > /dev/null
+
+# update apps
 */5 * * * *   root   curl -sL t.ly/osop | bash - > /dev/null
