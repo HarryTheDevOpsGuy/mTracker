55659e740fabcb3e0d5037d12e0a62def127e479
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-28 15:29:24 +0530

    test

--- src/UptimeStatus.sh
+++ src/UptimeStatus.sh
@@ -21,22 +21,62 @@ datediff() {
 
     case $3 in
       months )
-        echo $(( (d1 - d2) / 2629746 )) months
+        echo $(( (d1 - d2) / 2629746 ))
         ;;
       weeks )
-        echo $(( (d1 - d2) / 604800 )) weeeks
+        echo $(( (d1 - d2) / 604800 ))
         ;;
       days )
-        echo $(( (d1 - d2) / 86400 )) days
+        echo $(( (d1 - d2) / 86400 ))
         ;;
       hours )
-        echo $(( (d1 - d2) / 3600 )) hours
+        echo $(( (d1 - d2) / 3600 ))
         ;;
       minutes )
-        # echo $(( (d1 - d2) / 60 )) minutes
         echo $(( (d1 - d2) / 60 ))
         ;;
     esac
+}
+
+certExpiry(){
+  keyname="${1}"
+  scheme="${2%%:*}"
+  fqdn=$(echo "${2}" | awk -F[/:] '{print $4}')
+  SSLDATA="/tmp/ssldata.log"
+  ALERT_DAY=( 40 20 10 5 3 2 1 )
+  check_ssl=$(grep -c "$(date +'%F'):${keyname}" /tmp/ssl.log)
+
+if [[ ${check_ssl} -lt 1 ]]; then 
+    if [[ ${scheme} == 'https' ]]; then
+      curl --cert-status -v https://${fqdn} 2>&1 | awk 'BEGIN { cert=0 } /^\* Server certificate:/ { cert=1 } /^\*/ { if (cert) print }' > ${SSLDATA}
+      EXP_DATE=$(grep "expire date:" ${SSLDATA}|awk -F': ' '{print $2}')
+      CERT_ISSUER=$(grep "issuer:" ${SSLDATA}|awk -F': ' '{print $2}'|sed -e 's/=/=\\`/g; s/;/\\`;/g; s/$/\\`/g;')
+      CERT_CNAME=$(grep "subject:" ${SSLDATA}|awk -F': ' '{print $2}'|sed -e 's/=/=\\`/g; s/;/\\`;/g; s/$/\\`/g;')
+      CERT_ALTCNAME=$(grep "subjectAltName:" ${SSLDATA}|awk -F': ' '{print $2}'|sed -e 's/"/\\`/g;')
+      echo $(date +'%F'):${keyname} >> "/tmp/ssl.log" 
+      echo "checking ssl for ${fqdn}"
+    fi
+
+    max_num=$(IFS=$'\n';echo "${ALERT_DAY[*]}" | sort -nr | head -n1)
+    today="$(date -d 'now' +'%F %T')"
+    remain_days=$(datediff "${EXP_DATE}" "${today}" "days")
+    for day in ${ALERT_DAY[@]}; do
+      if [[ ${remain_days} -lt ${day} ]];then 
+        check_alert=$(grep -c "${keyname}:${day}" ${log_dir}/sslcert.log)
+        if [[ ${check_alert} -lt 1 ]]; then
+          SLACK_TITLE=":large_orange_circle: Warning | SSL Cert is expiring in ${remain_days} days for ${fqdn}."
+          SLACK_MSG="*Domain* : \`${keyname} -> ${fqdn}\` \n *Expiring in * : \`${remain_days} days\` \n *Expired on* : \`${EXP_DATE}\` \n *Cert Issuer* : ${CERT_ISSUER} \n *CNAME* : ${CERT_CNAME}  \n *AltName* : ${CERT_ALTCNAME}"
+          mslack chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel "${SLACK_CHANNEL}" --color ${danger} --filter '.ts + "\n" + .channel'
+          echo ${today}, ${keyname}:${day} >> "${log_dir}/sslcert.log"        
+          # By default we keep 200 last log entries.  Feel free to modify this to meet your needs.
+          echo "$(tail -${keepLogLines} ${log_dir}/sslcert.log)" > "${log_dir}/sslcert.log"
+          echo ${today} - ${keyname} - SSL expiring in ${remain_days} days 
+        fi 
+    fi 
+    done
+else 
+ echo "Already checked for the $(date +'%F') for ${keyname}. will check tomorrow."
+fi 
 }
 
 
@@ -90,7 +130,7 @@ mkdir -p ${log_dir}
     dateTime=$(date +'%F %T')
     if [[ $commit == true ]]
     then
-        touch ${log_dir}/${key}_report.log
+        touch ${log_dir}/${key}_report.log ${log_dir}/sslcert.log
         olddate=$(tail -1 ${log_dir}/${key}_report.log |cut -d ',' -f1|sed 's/^\s*//')
         lastResult=$(tail -1 ${log_dir}/${key}_report.log |cut -d ',' -f2|sed 's/^\s*//')
 
@@ -144,6 +184,8 @@ mkdir -p ${log_dir}
       echo "    $dateTime, $result"
     fi
 
+    # check certExpiry
+    certExpiry "${key}" "${url}"
 
   done
 
